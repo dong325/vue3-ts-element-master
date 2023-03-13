5ace49f86d7661e9b8f7edb374d99a1add090ee8
zhangsan <3375547357@qq.com>
2023-03-13 21:42:06 +0800

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="login-container w100 h100 f_c_c_c">
+    <div class="system-title">{{ title }}</div>
+    <el-form
+      ref="ruleFormRef"
+      :model="form"
+      :rules="rules"
+      label-width="70px"
+      class="ruleForm"
+      :size="formSize"
+      status-icon
+    >
+      <el-form-item label="用户名" prop="userName">
+        <el-input v-model="form.userName" />
+      </el-form-item>
+      <el-form-item label="密码" prop="passWord">
+        <el-input v-model="form.passWord" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">
+          登录
+        </el-button>
+        <el-button @click="resetForm(ruleFormRef)">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from "vue";
+import router from "../router";
+import type { FormInstance, FormRules } from "element-plus";
+import { setToken, removeToken } from "@/util/auth";
+
+const title = ref(import.meta.env.VITE_APP_TITLE);
+const formSize = ref("default");
+const ruleFormRef = ref<FormInstance>();
+const form = reactive({
+  userName: "",
+  passWord: "",
+});
+
+const rules = reactive<FormRules>({
+  userName: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+  passWord: [{ required: true, message: "请输入密码", trigger: "blur" }],
+});
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      setToken(Math.round(Math.random() * 100) + "");
+      router.push("/");
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+};
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+  removeToken();
+};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  background-image: url(@/assets/images/login-bg.jpg);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+
+  .system-title {
+    font-size: 30px;
+    color: #ffffff;
+    margin-top: -30px;
+    margin-bottom: 20px;
+  }
+
+  .ruleForm {
+    border-radius: 10px;
+    padding: 40px 50px 20px 20px;
+    background-color: rgba($color: #ffffff, $alpha: 1);
+    color: #ffffff;
+  }
+}
+</style>
---
+++ src/views/MenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-sub-menu :index="item.index" v-if="item.children && item.children.length">
+    <template #title>
+      <el-icon>
+        <location />
+      </el-icon>
+      <span>{{ item.title }}</span>
+    </template>
+    <menu-item
+      v-for="child in item.children"
+      :key="child.index"
+      :item="child"
+    ></menu-item>
+  </el-sub-menu>
+  <el-menu-item :index="item.index" v-else>
+    <el-icon><location /></el-icon>
+    <template #title>{{ item.title }}</template>
+  </el-menu-item>
+</template>
+
+<script lang="ts" setup>
+import { Location } from "@element-plus/icons-vue";
+// const props = defineProps({
+//         item: Object,
+// })
+
+// 给props指定默认值
+interface Props {
+  item: any;
+}
+withDefaults(defineProps<Props>(), {
+  item: () => ({}),
+});
+</script>
+
+<style lang="" scoped></style>
